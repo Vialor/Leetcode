85abb3e2d4c33743f9d9e126e147faf06a865e50
Yiyang Zhou <yiyang.zhou@mail.utoronto.ca>
2022-10-21 19:05:36 -0400

    1021

--- dynamic_programing.py
+++ dynamic_programing.py
@@ -1,3 +1,4 @@
+import functools
 import math
 from typing import List, Optional
 
@@ -17,10 +18,31 @@ class Solution:
       dp.append(max(dp[-1], dp[-2]+n))
     return dp[-1]
 
-  # 213
+  # 213*
   def rob2(self, nums: List[int]) -> int:
     if len(nums) == 1: return nums[0]
     return max(self.rob1(nums[1:]), self.rob1(nums[:-1]))
+
+  # 64*
+  def minPathSum(self, grid: List[List[int]]) -> int:
+    m = len(grid)
+    n = len(grid[0])
+    for i in range(m):
+      for j in range(n):
+        if i == 0 and j == 0: continue
+        if i == 0: grid[i][j] += grid[i][j-1]
+        elif j == 0: grid[i][j] += grid[i-1][j]
+        else: grid[i][j] += min(grid[i][j-1], grid[i-1][j])
+    return grid[m-1][n-1]
+
+  # 62
+  def uniquePaths(self, m: int, n: int) -> int:
+    # C(c1, c2)
+    c1 = m + n - 2
+    c2 = min(m, n) - 1
+    
+    contMult = lambda p, q : functools.reduce(lambda a, b:a * b, range(p, q), 1)
+    return int(contMult(c1 - c2 + 1, c1 + 1)/contMult(1, c2 + 1))
 
   # 300*
   def lengthOfLIS(self, nums: List[int]) -> int:

ad0db5765711c3365f96b8fbe88e8fe1c27552c6
Yiyang Zhou <yiyang.zhou@mail.utoronto.ca>
2023-04-11 21:10:03 +0800

    0411

--- linked_list.py
+++ linked_list.py
@@ -39,4 +39,51 @@ class Solution:
 
     # 445
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        stack1, stack2 = [], []
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
+
+        result = ListNode()
+        carry = 0
+        while stack1 or stack2 or carry:
+            stack1Val = stack1.pop() if stack1 else 0
+            stack2Val = stack2.pop() if stack2 else 0
+            newVal = stack1Val + stack2Val + carry
+            if newVal >= 10:
+                newVal -= 10
+                carry = 1
+            else:
+                carry = 0
+            newNode = ListNode(val=newVal, next=result.next)
+            result.next = newNode
+        return result.next
+
+    # 234
+    # time: O(N), space: O(1)
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        if not slow.next:
+            return True
+        if not slow.next.next:
+            return head.val == slow.next.val
+        halfLength = 1
+        tail, tailNext = slow.next, slow.next.next
+        while tailNext:
+            halfLength += 1
+            temp = tailNext.next
+            tailNext.next = tail
+            tail, tailNext = tailNext, temp
+
+        for i in range(halfLength):
+            if head.val != tail.val:
+                return False
+            head, tail = head.next, tail.next
+        return True
